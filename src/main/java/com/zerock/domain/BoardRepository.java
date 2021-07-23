3eaabd4109a45b2e990880bb014175991c421a55
HaSense <virtual_sense@naver.com>
2021-07-23 16:22:38 +0900

    Initial commit

--- src/main/java/com/zerock/domain/BoardRepository.java
+++ src/main/java/com/zerock/domain/BoardRepository.java
@@ -1,6 +1,10 @@
 package com.zerock.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -24,8 +28,27 @@ public interface BoardRepository extends CrudRepository<Board, Long> {
     public Collection<Board> findByTitleContainingAndBnoGreaterThan(
             String keyword, Long num);
 
-    //bno > ? ORDER BY bno DESC
+    // bno > ? ORDER BY bno DESC
     public Collection<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno);
 
+    // bno > ? ORDER BY bno DESC limit ?, ?
+    public List<Board> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
 
+    public Page<Board> findByBnoGreaterThan(Long bno, Pageable paging);
+
+//	@Query("select board from Board board where board.title like %?1% and board.bno > 0 order by board.bno desc")
+//	public List<Board> findByTitle(String title);
+
+    @Query("SELECT b FROM Board b WHERE b.title like %?1% and b.bno > 0 ORDER BY b.bno desc")
+    public List<Board> findByTitle(String title);
+
+    @Query("select board.bno, board.title, board.writer, board.regdate "
+            + " from Board board where board.title like %?1% and board.bno > 0 order by board.bno desc")
+    public List<Object[]> findByTitle2(String title);
+
+    @Query("select board from Board board where board.bno > 0 order by board.bno desc")
+    public List<Board> findBypage(Pageable pageable);
+
+    @Query("SELECT b from Board b WHERE b.content like %:content%  and b.bno > 0 order by b.bno desc")
+    public List<Board> findByContent(@Param("content") String content);
 }
--- src/test/java/com/zerock/Boot03ApplicationTests.java
+++ src/test/java/com/zerock/Boot03ApplicationTests.java
@@ -6,9 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -68,8 +74,58 @@ class Boot03ApplicationTests {
 
     @Test
     public void testBnoOrderBy() {
-        Collection<Board> results =
-                repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+        Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(90L);
         results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void testBnoOrderByPaging() {
+        //Pageable paging = new PageRequest(0, 10);
+
+        //spring boot 2.0.0
+        Pageable paging = PageRequest.of(0, 10);
+
+        Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+        results.forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void testBnoPagingSort() {
+
+        //Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+
+        //spring boot 2.0.0
+        Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+
+        Page<Board> result = repo.findByBnoGreaterThan(0L, paging);
+
+        System.out.println("PAGE SIZE: " + result.getSize());
+        System.out.println("TOTAL PAGES: " + result.getTotalPages());
+        System.out.println("TOTAL COUNT: " + result.getTotalElements());
+        System.out.println("NEXT: " + result.nextPageable());
+
+        List<Board> list = result.getContent();
+
+        list.forEach(board -> System.out.println(board));
+
+    }
+
+    @Test
+    public void testByTitle2() {
+        repo.findByTitle("17").forEach(board -> System.out.println(board));
+    }
+
+    @Test
+    public void testByTitle17() {
+        repo.findByTitle2("17").forEach(arr -> System.out.println(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void testByPaging() {
+        //Pageable pageable = new PageRequest(0, 10);
+
+        //new PageRequest --> PageRequest.of
+        Pageable pageable = PageRequest.of(0, 10);
+        repo.findBypage(pageable).forEach(board -> System.out.println(board));
     }
 }
